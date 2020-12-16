e65f17e87fa579d32d63bcbf9d765ea1ade1f073
vishv17 <vishvshroff54@gmail.com>
2020-12-16 21:24:10 +0530

    Basic Flutter App
    Show Images in App

--- lib/main.dart
+++ lib/main.dart
@@ -16,16 +16,13 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Text(
-          'Hello World',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey[600],
-            fontFamily: 'Montserrat',
-          ),
-        ),
+        /*child: Image(
+          // image: NetworkImage('https://images.unsplash.com/photo-1489549132488-d00b7eee80f1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'),
+          //Local Image
+          image: AssetImage('assets/background.jpg'),
+        )*/
+        // child: Image.asset('assets/img1.jpg'),
+        child: Image.network('https://images.unsplash.com/photo-1489549132488-d00b7eee80f1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: ()=>{},
